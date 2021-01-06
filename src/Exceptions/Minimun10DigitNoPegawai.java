f632049024bad1f8c6415be16ffcc0c14cae9dba
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-06 18:56:52 +0700

    feat(exceptions): create class to handle minimum input no pegawai

---
+++ src/Exceptions/Minimun10DigitNoPegawai.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exceptions;
+
+/**
+ *
+ * @author Alfian Andi Nugraha
+ */
+public class Minimun10DigitNoPegawai extends Exception {
+    public String getMessage() {
+        return "No Pegawai harus 10 digit";
+    }
+}
